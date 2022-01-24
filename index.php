c32b39273ba7258c486ad554c157ebb85856eebf
Pmnmpr <pmnmpr@gmail.com>
2022-01-24 18:39:21 +0000

    Obfuscated

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             61eef232cdbcf             |
+    |_______________________________________|
+*/
+ use Pmpr\Package\Mailer\Mailer; Mailer::symcgieuakksimmu();
---
+++ src/Pmpr/Package/Mailer/Asset.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             61eef232cdbcf             |
+    |_______________________________________|
+*/
+ namespace Pmpr\Package\Mailer; class Asset extends Container { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\x61\144\x6d\151\156\x5f\x69\x6e\x69\x74", [$this, "\x65\156\161\165\145\x75\x65"]); } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->ayeieigcckcmsikq($eygsasmqycagyayw->owygwqwawqoiusis("\164\145\x73\164\x2d\145\155\141\x69\154", $eygsasmqycagyayw->get("\x74\145\x73\164\56\152\x73"))->ayuciigykaswwqeo("\x6a\161\165\145\162\171")); $eygsasmqycagyayw->ikqyiskqaaymscgw("\x61\152\141\170", ["\x73\145\156\x64\137\x65\155\141\151\154" => Ajax::ykiigwasoeagkiuq]); } }
