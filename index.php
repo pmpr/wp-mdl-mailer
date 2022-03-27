520087c36b2d1164c3f23df53e80dfb393889b7b
Pmnmpr <pmnmpr@gmail.com>
2022-03-27 07:09:35 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             622a20223be78             |
+    |             62400d8c8da7a             |
     |_______________________________________|
 */
  use Pmpr\Package\Mailer\Mailer; Mailer::symcgieuakksimmu();
--- src/Pmpr/Package/Mailer/Asset.php
+++ src/Pmpr/Package/Mailer/Asset.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             622a20223be78             |
+    |             62400d8c8da7a             |
     |_______________________________________|
 */
- namespace Pmpr\Package\Mailer; class Asset extends Container { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\141\x64\x6d\151\x6e\x5f\x69\x6e\151\164", [$this, "\145\x6e\x71\165\145\165\x65"]); } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->ayeieigcckcmsikq($eygsasmqycagyayw->owygwqwawqoiusis("\x74\145\163\164\55\145\155\141\x69\154", $eygsasmqycagyayw->get("\x74\x65\x73\x74\x2e\152\163"))->ayuciigykaswwqeo("\x6a\x71\x75\x65\x72\171")); $eygsasmqycagyayw->ikqyiskqaaymscgw("\x61\152\141\170", ["\163\145\156\144\x5f\x65\155\141\151\154" => Ajax::ykiigwasoeagkiuq]); } }
+ namespace Pmpr\Package\Mailer; class Asset extends Container { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\x61\144\x6d\151\156\137\151\156\x69\164", [$this, "\x65\x6e\x71\165\145\x75\145"]); } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->ayeieigcckcmsikq($eygsasmqycagyayw->owygwqwawqoiusis("\164\145\x73\164\x2d\x65\x6d\x61\x69\154", $eygsasmqycagyayw->get("\164\145\163\x74\x2e\x6a\163"))->ayuciigykaswwqeo("\152\x71\165\145\x72\171")); $eygsasmqycagyayw->ikqyiskqaaymscgw("\141\x6a\x61\x78", ["\163\145\x6e\x64\x5f\x65\155\141\x69\x6c" => Ajax::ykiigwasoeagkiuq]); } }
