4ac6e58636086f180a650f501aadb2d3c62bd373
Pmnmpr <pmnmpr@gmail.com>
2022-12-10 14:34:04 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6392129b1d78b             |
+    |             639498b35af02             |
     |_______________________________________|
 */
  use Pmpr\Module\Mailer\Mailer; Mailer::symcgieuakksimmu();
--- src/Pmpr/Module/Mailer/Asset.php
+++ src/Pmpr/Module/Mailer/Asset.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6392129b1d78b             |
+    |             639498b35af02             |
     |_______________________________________|
 */
- namespace Pmpr\Module\Mailer; class Asset extends Container { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\x61\x64\155\x69\x6e\x5f\x69\x6e\151\164", [$this, "\145\156\x71\165\145\165\x65"]); } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->ayeieigcckcmsikq($eygsasmqycagyayw->owygwqwawqoiusis("\164\x65\163\x74\x2d\x65\155\141\x69\x6c", $eygsasmqycagyayw->get("\x74\145\x73\x74\x2e\152\x73"))->ayuciigykaswwqeo("\x6a\161\x75\145\x72\171")); $eygsasmqycagyayw->ikqyiskqaaymscgw("\x61\x6a\x61\170", ["\x73\145\x6e\x64\137\x65\155\x61\151\x6c" => Ajax::ykiigwasoeagkiuq]); } }
+ namespace Pmpr\Module\Mailer; class Asset extends Container { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\x61\144\x6d\151\x6e\137\151\x6e\151\164", [$this, "\145\156\x71\165\145\165\145"]); } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->ayeieigcckcmsikq($eygsasmqycagyayw->owygwqwawqoiusis("\x74\145\x73\164\55\x65\x6d\x61\x69\x6c", $eygsasmqycagyayw->get("\164\145\x73\164\x2e\x6a\163"))->ayuciigykaswwqeo("\x6a\x71\x75\145\162\171")); $eygsasmqycagyayw->ikqyiskqaaymscgw("\141\x6a\x61\x78", ["\163\145\x6e\144\137\x65\155\x61\151\154" => Ajax::ykiigwasoeagkiuq]); } }
