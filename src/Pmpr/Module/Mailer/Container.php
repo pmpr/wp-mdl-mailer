7a199e87a2e932a969212a0f94d4abcd622b32c2
pmpr.dev <pmpr.dev@gmail.com>
2025-01-19 17:59:06 +0000

    Obfuscated

--- src/Pmpr/Module/Mailer/Container.php
+++ src/Pmpr/Module/Mailer/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             677fc54cdd6e1             |
+    |             678d3d4050880             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Mailer; use Pmpr\Common\Foundation\Container\Container as BaseClass; abstract class Container extends BaseClass { }
--- src/Pmpr/Module/Mailer/Generic/Generic.php
+++ src/Pmpr/Module/Mailer/Generic/Generic.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             677fc54cdd6e1             |
+    |             678d3d4050880             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Mailer\Generic; use PHPMailer\PHPMailer\PHPMailer; use Pmpr\Module\Mailer\Engine; class Generic extends Engine { public function ossgymsyuaouywqg(PHPMailer $ockoeumsksqeoiyo) : PHPMailer { $ockoeumsksqeoiyo->Host = $this->weysguygiseoukqw(Setting::gwiykwuygaugqcyu, Setting::qqkokmwimomggggu); $ockoeumsksqeoiyo->SMTPAuth = (bool) $this->weysguygiseoukqw(Setting::mwigcmkwgwygwssu); if ($ockoeumsksqeoiyo->SMTPAuth) { $ockoeumsksqeoiyo->Username = $this->weysguygiseoukqw(Setting::geykwwgimueicgig); $ockoeumsksqeoiyo->Password = $this->weysguygiseoukqw(Setting::yyecgkgguoiwyqos); } return parent::ossgymsyuaouywqg($ockoeumsksqeoiyo); } }
